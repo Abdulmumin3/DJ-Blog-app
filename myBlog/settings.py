7790ee91ff2fe4af40237eaa25bcfa3d4a1583d7
Adesoye <abdulmumeenadesoye19@gmail.com>
2023-05-31 22:27:08 +0100

    Latest commit

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 
 # Application definition
