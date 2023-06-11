5a9da491b75241828f2886b26cd99aa44aadd829
Adesoye <abdulmumeenadesoye19@gmail.com>
2023-06-11 16:08:31 +0100

    Another commit

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = "myBlog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": 8044,
     }
 }
 
