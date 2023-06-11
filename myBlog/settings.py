78fbff4417d162dad010e9c9ada23ed78efe23c0
Adesoye <abdulmumeenadesoye19@gmail.com>
2023-06-11 15:03:36 +0100

    Latest commit

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'theme',
     'django_browser_reload',
     'imagekit',
-    "corsheaders"
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "corsheaders.middleware.CorsPostCsrMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
